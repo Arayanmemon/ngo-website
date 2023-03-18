fe7cf8ec5296722f2465c3aa68822994e56e19ca
fahadzardari <fahadzardari111@gmail.com>
2023-03-18 08:02:52 +0500

    sidebar menu item added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import hamburger from "/icons/hamburg.png";
+import appLogo from "/logos/Al-fouz.png";
+import dashBoardIcon from "/icons/dashboard-icon.png";
 export default function App() {
   const [open, setOpen] = useState();
   return (
@@ -9,24 +12,36 @@ export default function App() {
           "linear-gradient(180deg, #1C244D 0%, rgba(58, 143, 143, 0) 100%)",
       }}
     >
-
       {/* SIDEAR */}
       <div className="flex flex-row">
         <div
-          className={`h-screen bg-blue-200 px-5 ${
+          className={`h-screen bg-blue-200 px-4  ${
             open ? "w-72" : "w-0 px-0"
           } duration-700 `}
         >
-          <div className={`${open ? "flex" : "hidden"} flex-col px-4`}>
-              sidebar options
+          <div className={`${open ? "flex" : "hidden"} flex flex-col w-full `}>
+            <div><img src={appLogo} alt="" /></div>
+
+            {/* Side BAR MENU CONTENTS */}
+
+            <div className="flex flex-row justify-between px-4 text-xl items-center cursor-pointer ">
+              <img src={dashBoardIcon} alt="" />
+              <div >
+                 DASHBOARD
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="w-full">
-            {/* NAVBAR */}
+          {/* NAVBAR */}
           <div className="flex flex-row w-full px-10 justify-between">
             <div className="flex flex-row">
-              <div><button onClick={() => setOpen(!open)}>Open</button></div>
+              <div>
+                <button onClick={() => setOpen(!open)}>
+                  <img src={hamburger} className="w-9" />
+                </button>
+              </div>
               <div>Menu</div>
               <div>
                 <input type="text" />
@@ -38,16 +53,10 @@ export default function App() {
             </div>
           </div>
 
+          {/* Main Contents */}
 
-            {/* Main Contents */}
-        
-            <div>
-
-              
-            </div>
+          <div></div>
         </div>
-
-      
       </div>
     </div>
   );
