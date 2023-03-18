06a0d4e5dfcfb21b52451435d53660cda78653b0
Fahad Hussain <fahadzardari111@gmail.com>
2023-03-19 02:50:03 +0500

    basic structure of my pages done styling remains

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,24 +3,76 @@ import appLogo from "/logos/Al-fouz.png";
 import dashBoardIcon from "/icons/dashboard-icon.png";
 import { useState } from "react";
 import hamburger from "/icons/hamburg.png";
-
+import { useNavigate } from "react-router-dom";
+import profileIcon from "/icons/profile-icon.png";
 
 export default function ({ navRequired }) {
+  const navigate = useNavigate();
+  function handleClick(x) {
+    navigate(x);
+  }
+  const [projectsSlide , setProjectsSlide] = useState(false)
   return (
     <div className="flex flex-row">
       <div
         className={`h-screen bg-blue-200 ${
-          open ? "w-72 px-4" : "w-0 px-0"
+          open ? "w-72 " : "w-0 px-0"
         } duration-700 `}
       >
-        <div className={`${open ? "flex" : "hidden"} flex flex-col w-full `}>
+        <div
+          className={`${
+            open ? "flex" : "hidden"
+          } flex flex-col w-full text-white text-2xl capitalize font-bold `}
+        >
           <div>
             <img src={appLogo} alt="" />
           </div>
           {/* Side BAR MENU CONTENTS */}
-          <div className="flex flex-row justify-between px-4 text-xl items-center cursor-pointer ">
+          <div
+            className="flex flex-row px-4 py-2 hover:bg-blue-400 justify-left items-center cursor-pointer "
+            onClick={() => handleClick("/")}
+          >
             <img src={dashBoardIcon} alt="" />
             <div>DASHBOARD</div>
+          </div>
+          <div
+            className="flex flex-row px-4 py-2 hover:bg-blue-400 justify-left items-center cursor-pointer "
+            onClick={() => handleClick("/profile")}
+          >
+            <img src={profileIcon} alt="" />
+            <div>My Profile</div>
+          </div>
+
+          <div
+            className="flex flex-row px-4 py-2 hover:bg-blue-400 justify-left items-center cursor-pointer "
+            onClick={() => setProjectsSlide(!projectsSlide)}
+          >
+            <img src={profileIcon} alt="" />
+            <div>Projects</div>
+          </div>
+            <div className={`pl-10 ${projectsSlide ? 'relative ' : 'hidden h-0'} duration-300`}>
+
+                <div className="cursor-pointer" onClick={() => handleClick("/addprojects")}>
+                  <p>Add Projects</p>
+                </div>
+                
+                <div className="cursor-pointer" onClick={() => handleClick("/yourprojects")}>
+                  <p>View Projects</p>
+                </div>
+            </div>
+          <div
+            className="flex flex-row px-4 py-2 hover:bg-blue-400 justify-left items-center cursor-pointer "
+            onClick={() => handleClick("/volunteers")}
+          >
+            <img src={profileIcon} alt="" />
+            <div>Volunteers</div>
+          </div>
+          <div
+            className="flex flex-row px-4 py-2 hover:bg-blue-400 justify-left items-center cursor-pointer "
+            onClick={() => handleClick("/awards")}
+          >
+            <img src={profileIcon} alt="" />
+            <div>Awards</div>
           </div>
         </div>
       </div>
--- src/pages/AddProjects.jsx
+++ src/pages/AddProjects.jsx
@@ -1,13 +1,58 @@
 import Sidebar from "../components/Sidebar";
-
+import clipboardIcon from "/icons/clipboard-icon.png"
 import React from 'react'
 
 export default function AddProjects() {
   return (
         <div className="flex flex-row">
                 <Sidebar />
-                <div>
-add projects
+                <div className="w-full mt-4">
+                        <div className="w-full items-center pl-4 text-6xl bg-gray-300 py-2 text-white flex ">
+                               <img src={clipboardIcon} alt="" /> <h1 className="pl-4">Add Project</h1>
+                        </div>
+
+                        <div className="pl-20 w-4/6 text-3xl mt-16 text-white">
+                                <ul>
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">Name</h1>
+                                                <input type="text"  className="rounded-lg  text-2xl px-2 py-1 w-2/5 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br />
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">Location</h1>
+                                                <input type="text"  className="rounded-lg  text-2xl px-2 py-1 w-2/5 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br />
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">Description</h1>
+                                                <input type="text"  className="rounded-lg  text-2xl px-2 py-1 w-2/5 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br />
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">Start Date</h1>
+                                                <input type="date" placeholder="Input Start Date"  className="rounded-lg w-2/5  text-2xl px-2 py-1 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br />
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">End Date</h1>
+                                                <input type="date" placeholder="Input End Date" className="rounded-lg w-2/5 text-2xl px-2 py-1 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br />
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">Service Category</h1>
+                                                <input type="text"  className="rounded-lg  text-2xl px-2 py-1 w-2/5 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br />
+                                        <li className="flex justify-between items-center">
+                                                <h1 className="font-bold">Project Cover</h1>
+                                                <input type="text"  className="rounded-lg  text-2xl px-2 py-1 w-2/5 bg-transparent border-2 border-white" />
+                                        </li>
+                                        <br /><br />
+                                        <li className="flex justify-center">
+                                                        <button className="text-white bg-slate-800 font-bold px-16 rounded-md py-1">SAVE</button>
+                                        </li>
+                                </ul>
+                        </div>
                 </div>
         </div>
   )
